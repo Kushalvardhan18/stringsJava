865d9f35488359f9e9c6a96e39bcae84b86188ac
Kushal Vardhan <vardhank140102@gmail.com>
2024-06-18 21:20:13 +0530

    Update StringsInJava.java

--- StringsInJava.java
+++ StringsInJava.java
@@ -1,6 +1,10 @@
 public class StringsInJava {
     public static void main(String[] args){
+//        Strings are Immutable :---------->
+
         String str ="abcd";
         System.out.println(str);
+        String str2 = new String("xyz");
+        System.out.println(str2 );
     }
 }
