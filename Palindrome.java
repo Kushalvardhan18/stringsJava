92b56fe6dff71c61a71fcdb1abd64e6816f65933
Kushal Vardhan <vardhank140102@gmail.com>
2024-06-21 19:56:02 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,16 @@
 public class Palindrome {
+    public static void check(String[] str){
+        int n = str.length;
+        for(int i =0;i<n/2;i++){
+            for(int j=n;j>0;j--){
+                if(str[i] == str[j]){
+
+                }
+            }
+        }
+    }
     public static void main(String[] args){
-        
+        String [] str = {"racecar","noon","madam"};
+        check(str);
     }
 }
