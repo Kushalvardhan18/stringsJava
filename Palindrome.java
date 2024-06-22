111fedbc23bd1e33be3ec8c8c1323f162886ac01
Kushal Vardhan <vardhank140102@gmail.com>
2024-06-22 11:22:54 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,16 +1,27 @@
 public class Palindrome {
     public static boolean isPalindrome(String str){
-        for(int i =0;i<str.length()/2;i++) {
+//        for(int i =0;i<str.length()/2;i++) {
+//            int n = str.length();
+//            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+//               return true;
+//            }
+//        }
+//        return false;
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length()-1;i>=0;i--){
             int n = str.length();
-            if (str.charAt(i) != str.charAt(n - 1 - i)) {
-               return true;
-            }
+            sb.append(str.charAt(i));
+        }
+        String strNew = sb.toString();
+
+        if(strNew.equals(str)){
+            return true;
         }
         return false;
     }
 
     public static void main(String[] args) {
-        String str = "noon";
+        String str = "false";
         System.out.println( isPalindrome(str));
     }
 }
