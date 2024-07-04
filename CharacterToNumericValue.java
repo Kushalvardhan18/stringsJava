f575dfe194cce0af9dba9d925a2f383deb4d8bfc
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-04 21:22:54 +0530

    Update CharacterToNumericValue.java

--- CharacterToNumericValue.java
+++ CharacterToNumericValue.java
@@ -1,5 +1,13 @@
 public class CharacterToNumericValue {
 public static void main(String[] args){
-    
+    String sc = "0000007777F4566";
+    for(int i =0;i<sc.length();i++){
+        if(Character.getNumericValue(sc.charAt(4))==0 ){
+            System.out.println("Kushal");
+        }
+        else{
+            System.out.println("Vardhan");
+        }
+    }
 }
 }
