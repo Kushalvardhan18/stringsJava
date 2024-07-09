6c73aeb1562f456fe9867677cac1bc7f43061315
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-09 20:52:54 +0530

    Update PracticeQuestions.java

--- PracticeQuestions.java
+++ PracticeQuestions.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class PracticeQuestions {
+    public static void lowerCaseVowels(String a){
+        for(int i =0)
+    }
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        lowerCaseVowels(a);
     }
 }
