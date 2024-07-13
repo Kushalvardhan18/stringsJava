486e876574d0ea1dae8c8e65212449fefcc74e78
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-13 20:43:34 +0530

    Update PracticeQuestions.java

--- PracticeQuestions.java
+++ PracticeQuestions.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PracticeQuestions {
@@ -10,13 +11,25 @@ public class PracticeQuestions {
         }
         return count;
     }
-    public static void isAnagram(){
-
+    public static boolean isAnagram(char[] str1, char[] str2){
+        int n1= str1.length;
+        int n2= str2.length;
+        if(n1 != n2){
+            return false;
+        }
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        for(int i =0;i<n1;i++){
+            if(str1[i] != str2[i]){
+                return false;
+            }
+        }
+        return true;
     }
     public static void countWords(String[] sentences){
         int maxNum =0;
         for(String sentence :sentences){
-            String [] words = sentence.split(" ");
+            String [] words = sentence.split(",");
             maxNum = Math.max(maxNum, words.length);
         }
         System.out.println(maxNum);
@@ -28,5 +41,8 @@ public class PracticeQuestions {
         System.out.println(lowerCaseVowels(a));
         String [] sentences = {"i","am a","very","talented","person"};
         countWords(sentences);
+        char[] str1 = {'c','a','r','e'};
+        char[] str2 = {'r','a','c','e'};
+        System.out.println(isAnagram(str1,str2));
     }
 }
