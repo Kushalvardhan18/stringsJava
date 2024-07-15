e3d07165ee4372b2ea47ceb5b53a509e71a5bff9
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-15 20:41:30 +0530

    Update StringsCompression.java

--- StringsCompression.java
+++ StringsCompression.java
@@ -3,20 +3,34 @@ public class StringsCompression{
         // "aaabbcccdd" = "a3b2c3d2"
         String newStr ="";
 
+//        for(int i =0;i<sc.length();i++){
+//            Integer count =1;
+//            char c = sc.charAt(i);
+//
+//            while(i<sc.length()-1 && sc.charAt(i) == sc.charAt(i+1)){
+//                count++;
+//                i++;
+//            }
+//            newStr += sc.charAt(i);
+//            if(count >1){
+//                newStr +=count.toString();
+//            }
+//        }
+//
+        StringBuilder str = new StringBuilder(sc.length());
         for(int i =0;i<sc.length();i++){
-            Integer count =1;
+            int count =1;
             char c = sc.charAt(i);
-
             while(i<sc.length()-1 && sc.charAt(i) == sc.charAt(i+1)){
                 count++;
                 i++;
             }
-            newStr += sc.charAt(i);
-            if(count >1){
-                newStr +=count.toString();
+            str.append(c);
+            if(count>1){
+                str.append(count);
             }
         }
-        return newStr;
+        return str.toString();
     }
     public static void main(String[] args){
        String sc ="aaabbcccdd";
