e7a0079a406b6e06a348fc040288751b84f9e6a7
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-15 20:35:30 +0530

    Update StringsCompression.java

--- StringsCompression.java
+++ StringsCompression.java
@@ -1,5 +1,25 @@
 public class StringsCompression{
+    public static String stringAfterCompression(String sc){
+        // "aaabbcccdd" = "a3b2c3d2"
+        String newStr ="";
+
+        for(int i =0;i<sc.length();i++){
+            Integer count =1;
+            char c = sc.charAt(i);
+
+            while(i<sc.length()-1 && sc.charAt(i) == sc.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newStr += sc.charAt(i);
+            if(count >1){
+                newStr +=count.toString();
+            }
+        }
+        return newStr;
+    }
     public static void main(String[] args){
-        
+       String sc ="aaabbcccdd";
+        System.out.println( stringAfterCompression(sc));
     }
 }
